68ff4a60912d5bccd53eab61632d04b69b540728
Luiz Gustavo <lglmoura@gmail.com>
2012-10-02 10:37:40 -0300

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locadora::Application.config.secret_token = '21e491fcf9e9c7d055a7003f214498afbc95e258d5e651660915b73796f4199b13cc78a141eac952139caabbc177afafe3ffa30975beb11cb069512298bde4c7'
